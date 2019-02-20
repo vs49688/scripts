f220a94867bea5b3613d61ca3605e6f0e5f2f460
Zane van Iperen <z.vaniperen@uq.edu.au>
2019-02-20 15:41:44 +1000

    i3setup: Add support for workstation

--- i3setup.sh
+++ i3setup.sh
@@ -2,12 +2,16 @@
 ##
 # Source in ~/.profile to handle switching from MATE<->i3
 #
-# Made for a Dell XPS 15 9550
+# In TWILYSPARKLE (Dell XPS 15 9550)
+# - Enable "Natural Scrolling" if using i3.
 # - Main Display is eDP-1
 # - Touchpad ID is 06CB:7A13
 #
+# In ZANE-DEVPC:
+# - Fix the monitor arrangement
 ##
-RESOLUTION=$(xrandr -q | grep eDP-1 | sed -nr 's/^.+primary\s+([0-9]+x[0-9]+).+$/\1/p')
+
+RESOLUTION=$(xrandr -q | sed -nr 's/^.+primary\s+([0-9]+x[0-9]+).+$/\1/p')
 
 if [ "$XDG_CURRENT_DESKTOP" = "MATE" ]; then
 	unset GDK_SCALE
@@ -24,11 +28,12 @@ if [ "$XDG_CURRENT_DESKTOP" = "MATE" ]; then
 elif [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
 	# Enable "natural" scrolling
 	TOUCHPAD_ID=$(xinput list | sed -nr 's/^.+06CB:7A13.+id=([0-9]+).+$/\1/p')
-	NS_ID=$(xinput list-props $TOUCHPAD_ID | sed -nr 's/^.+libinput\sNatural\sScrolling\sEnabled\s\(([0-9]+)\):.+$/\1/p')
-
-	xinput set-prop $TOUCHPAD_ID $NS_ID 1
+	if [ ! -z $TOUCHPAD_ID ]; then
+		NS_ID=$(xinput list-props $TOUCHPAD_ID | sed -nr 's/^.+libinput\sNatural\sScrolling\sEnabled\s\(([0-9]+)\):.+$/\1/p')
+		xinput set-prop $TOUCHPAD_ID $NS_ID 1
+		unset NS_ID
+	fi
 	unset TOUCHPAD_ID
-	unset NS_ID
 
 	if [ "$RESOLUTION" = "3840x2160" ]; then
 		export GDK_SCALE=2
@@ -46,3 +51,12 @@ elif [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
 fi
 
 unset RESOLUTION
+
+# Fix workstation's monitors
+if [ "$(hostname)" = "ZANE-DEVPC" ]; then
+	xrandr --output DP-4 --right-of DP-0
+	xrandr --output DP-7 --right-of DP-4
+fi
+
+export GTK_THEME=Arc-Dark
+
