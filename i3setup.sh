7ef496be3708adc2669c634675472d2a04798f6c
Zane van Iperen <zane@zanevaniperen.com>
2019-02-25 18:40:02 +1000

    enable horizontal scrolling

--- i3setup.sh
+++ i3setup.sh
@@ -3,13 +3,39 @@
 # Source in ~/.profile to handle switching from MATE<->i3
 #
 # In TWILYSPARKLE (Dell XPS 15 9550)
-# - Enable "Natural Scrolling" if using i3.
+# - Enable natural and horizontal scrolling
 # - Main Display is eDP-1
 # - Touchpad ID is 06CB:7A13
 #
 # In ZANE-DEVPC:
 # - Fix the monitor arrangement
 ##
+
+if [ "$(hostname)" = "ZANE-DEVPC" ]; then
+	# Fix the workstation's monitors
+	xrandr --output DP-4 --right-of DP-0
+	xrandr --output DP-7 --right-of DP-4
+	xrandr --output DP-4 --primary
+elif [ "$(hostname)" = "TWILYSPARKLE" ]; then
+	xrandr --output eDP-1 --primary
+
+	TOUCHPAD_ID=$(xinput list | sed -nr 's/^.+06CB:7A13.+id=([0-9]+).+$/\1/p')
+	if [ ! -z $TOUCHPAD_ID ]; then
+		# Enable "natural" scrolling
+		PROP_ID=$(xinput list-props $TOUCHPAD_ID | sed -nr 's/^.+libinput\sNatural\sScrolling\sEnabled\s\(([0-9]+)\):.+$/\1/p')
+		if [ ! -z $PROP_ID ]; then
+			xinput set-prop $TOUCHPAD_ID $PROP_ID 1
+		fi
+
+		# Enable horizontal scrolling
+		PROP_ID=$(xinput list-props $TOUCHPAD_ID | sed -nr 's/^.+libinput\sHorizontal\sScroll\sEnabled\s\(([0-9]+)\):.+$/\1/p')
+		if [ ! -z $PROP_ID ]; then
+			xinput set-prop $TOUCHPAD_ID $PROP_ID 1
+		fi
+		unset PROP_ID
+	fi
+	unset TOUCHPAD_ID
+fi
 
 RESOLUTION=$(xrandr -q | sed -nr 's/^.+primary\s+([0-9]+x[0-9]+).+$/\1/p')
 
@@ -26,15 +52,6 @@ if [ "$XDG_CURRENT_DESKTOP" = "MATE" ]; then
 		dconf write /org/mate/desktop/interface/window-scaling-factor 1
 	fi
 elif [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
-	# Enable "natural" scrolling
-	TOUCHPAD_ID=$(xinput list | sed -nr 's/^.+06CB:7A13.+id=([0-9]+).+$/\1/p')
-	if [ ! -z $TOUCHPAD_ID ]; then
-		NS_ID=$(xinput list-props $TOUCHPAD_ID | sed -nr 's/^.+libinput\sNatural\sScrolling\sEnabled\s\(([0-9]+)\):.+$/\1/p')
-		xinput set-prop $TOUCHPAD_ID $NS_ID 1
-		unset NS_ID
-	fi
-	unset TOUCHPAD_ID
-
 	if [ "$RESOLUTION" = "3840x2160" ]; then
 		export GDK_SCALE=2
 		export QT_SCALE_FACTOR=2
@@ -51,13 +68,6 @@ elif [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
 fi
 
 unset RESOLUTION
-
-# Fix workstation's monitors
-if [ "$(hostname)" = "ZANE-DEVPC" ]; then
-	xrandr --output DP-4 --right-of DP-0
-	xrandr --output DP-7 --right-of DP-4
-	xrandr --output DP-4 --primary
-fi
 
 export GTK_THEME=Arc-Dark
 
