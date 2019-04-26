ab4b5a078c7f30645a517a39f48f9386cc9d6e21
Zane van Iperen <zane@zanevaniperen.com>
2019-04-26 13:19:32 +1000

    i3setup.sh: cleanups

--- i3setup.sh
+++ i3setup.sh
@@ -18,47 +18,41 @@ if [ "$(hostname)" = "DEMORGOGON" ]; then
 	xrandr --output DP-7 --right-of DP-4
 	xrandr --output DP-4 --primary
 elif [ "$(hostname)" = "TWILYSPARKLE" ]; then
-	xrandr --output eDP-1 --primary --mode 3840x2160
+	# xrandr --output eDP-1 --primary --mode 3840x2160
+	# if [ ! -z "$(xrandr | grep 'HDMI-1 connected')" ]; then
+	# 	xrandr --output HDMI-1 --mode 1920x1080 --scale-from 3840x2160 --panning 3840x2160+3840+0 --right-of eDP-1
+	# fi
 
-	# If the external's connected, set it up
+	# Drop to 1080p so the system's actually usable.
+	xrandr --output eDP-1 --primary --mode 1920x1080
 	if [ ! -z "$(xrandr | grep 'HDMI-1 connected')" ]; then
-		xrandr --output HDMI-1 --mode 1920x1080 --scale-from 3840x2160 --panning 3840x2160+3840+0 --right-of eDP-1
+		xrandr --output HDMI-1 --mode 1920x1080 --right-of eDP-1
 	fi
 
-	TOUCHPAD_ID=$(xinput list | sed -nr 's/^.+06CB:7A13.+id=([0-9]+).+$/\1/p')
-	if [ ! -z $TOUCHPAD_ID ]; then
-		# Enable "natural" scrolling
-		PROP_ID=$(xinput list-props $TOUCHPAD_ID | sed -nr 's/^.+libinput\sNatural\sScrolling\sEnabled\s\(([0-9]+)\):.+$/\1/p')
-		if [ ! -z $PROP_ID ]; then
-			xinput set-prop $TOUCHPAD_ID $PROP_ID 1
-		fi
-
-		# Enable horizontal scrolling
-		PROP_ID=$(xinput list-props $TOUCHPAD_ID | sed -nr 's/^.+libinput\sHorizontal\sScroll\sEnabled\s\(([0-9]+)\):.+$/\1/p')
-		if [ ! -z $PROP_ID ]; then
-			xinput set-prop $TOUCHPAD_ID $PROP_ID 1
-		fi
-		unset PROP_ID
+	TOUCHPAD_ID=$(xinput list --id-only 'DLL06E4:01 06CB:7A13 Touchpad')
+	if [ ! -z ${TOUCHPAD_ID} ]; then
+		xinput set-prop ${TOUCHPAD_ID} 'libinput Natural Scrolling Enabled' 1
+		xinput set-prop ${TOUCHPAD_ID} 'libinput Horizontal Scroll Enabled' 1
 	fi
 	unset TOUCHPAD_ID
 fi
 
 RESOLUTION=$(xrandr -q | sed -nr 's/^.+primary\s+([0-9]+x[0-9]+).+$/\1/p')
 
-if [ "$XDG_CURRENT_DESKTOP" = "MATE" ]; then
+if [ "${XDG_CURRENT_DESKTOP}" = "MATE" ]; then
 	unset GDK_SCALE
 	xrandr --dpi 96
 	gsettings set org.gnome.desktop.interface scaling-factor 1
 
-	if [ "$RESOLUTION" = "3840x2160" ]; then
+	if [ "${RESOLUTION}" = "3840x2160" ]; then
 		export QT_SCALE_FACTOR=2
 		dconf write /org/mate/desktop/interface/window-scaling-factor 2
 	else
 		export QT_SCALE_FACTOR=1
 		dconf write /org/mate/desktop/interface/window-scaling-factor 1
 	fi
-elif [ "$XDG_CURRENT_DESKTOP" = "i3" ]; then
-	if [ "$RESOLUTION" = "3840x2160" ]; then
+elif [ "${XDG_CURRENT_DESKTOP}" = "i3" ]; then
+	if [ "${RESOLUTION}" = "3840x2160" ]; then
 		export GDK_SCALE=2
 		export QT_SCALE_FACTOR=2
 		xrandr --dpi 235
