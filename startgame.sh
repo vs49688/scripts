cf6e5f62f52aad1bba9b521b7f921077ec6a6f25
Zane van Iperen <zane@zanevaniperen.com>
2020-05-22 22:18:36 +1000

    startgame.sh: update paths

--- startgame.sh
+++ startgame.sh
@@ -5,44 +5,36 @@
 ##
 
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 <trl|trldebug|tra|tru|tr2013|eso|lol|ds2|me1|bl|ac1|nier>"
+	echo "Usage: $0 <trl|trldebug|tra|tru|tr2013|eso|ds2|bl|ac1|nier>"
 	exit 2
 fi
 
-#xmessage -print -buttons trl,trldebug,tra,tru,tr2013,eso,lol,ds2,me1 "Select a game"
-
 if [ $1 = "trl" ]; then
 	export WINEPREFIX=${HOME}/.wine-trl
-	cd /media/Data2/Games/Tomb\ Raider\ Legend
+	cd ${HOME}/Games/Tomb\ Raider\ Legend
 	exec wine trl.exe
 elif [ $1 = "trldebug" ]; then
 	export WINEPREFIX=${HOME}/.wine-trl
-	cd /media/Data2/Games/Tomb\ Raider\ Legend
+	cd ${HOME}/Games/Tomb\ Raider\ Legend
 	exec wine tr7.exe
 elif [ $1 = "tra" ]; then
 	export WINEPREFIX=${HOME}/.wine-trl
-	cd /media/Data2/Games/Tomb\ Raider\ Anniversary
+	cd ${HOME}/Games/Tomb\ Raider\ Anniversary
 	exec wine tra.exe
 elif [ $1 = "tru" ]; then
 	export WINEPREFIX=${HOME}/.wine-trl
-	cd /media/Data2/Games/Tomb\ Raider\ Underworld
+	cd ${HOME}/Games/Tomb\ Raider\ Underworld
 	exec wine tru.exe
 elif [ $1 = "tr2013" ]; then
 	export WINEPREFIX=${HOME}/.wine-tr2013
-	cd /media/Data2/Games/Tomb\ Raider\ GOTY
+	cd ${HOME}/Games/Tomb\ Raider\ GOTY
 	exec wine TombRaider.exe
 elif [ $1 = "eso" ]; then
 	export WINEPREFIX=${HOME}/.wine-eso
-	exec wine /media/Data2/Games/Zenimax\ Online/Launcher/Bethesda.net_Launcher.exe
-elif [ $1 = "lol" ]; then
-	export WINEPREFIX=${HOME}/.wine-lol
-	exec wine /media/Data2/Games/League\ of\ Legends/LeagueClient.exe
+	exec wine ${HOME}/Games/Zenimax\ Online/Launcher/Bethesda.net_Launcher.exe
 elif [ $1 = "ds2" ]; then
 	export WINEPREFIX=${HOME}/.wine-ds2
-	exec wine /media/Data2/Games/Dark\ Souls\ 2\ -\ Scholar\ of\ the\ First\ Sin/DarkSoulsII.exe
-elif [ $1 = "me1" ]; then
-	export WINEPREFIX=/media/Data2/wine/masseffect
-	exec wine /media/Data2/wine/masseffect/drive_c/Games/Mass\ Effect/Binaries/MassEffect.exe
+	exec wine ${HOME}/Games/Dark\ Souls\ 2\ -\ Scholar\ of\ the\ First\ Sin/DarkSoulsII.exe
 elif [ $1 = "bl" ]; then
 	export WINEPREFIX=${HOME}/.wine-wow
 	wine reg ADD HKCU\\Software\\Wine\\DllOverrides /t REG_SZ /f /v "ucrtbase" /d "native,builtin"
@@ -51,19 +43,19 @@ elif [ $1 = "bl" ]; then
 	# xmodmap -e "keycode 37 = Control_R"
 	exec wine ${HOME}/Games/Battle.net/Battle.net.exe
 elif [ $1 = "ac1" ]; then
-	export WINEPREFIX=/media/Data2/Games/AssassinsCreed/wine
-	cd /media/Data2/Games/AssassinsCreed
-	exec wine /media/Data2/Games/AssassinsCreed/AssassinsCreed_Dx10.exe
+	export WINEPREFIX=${HOME}/Games/AssassinsCreed/wine
+	cd ${HOME}/Games/AssassinsCreed
+	exec wine ${HOME}/Games/AssassinsCreed/AssassinsCreed_Dx10.exe
 elif [ $1 = "nier" ]; then
 	export LANG=en_AU.UTF-8
 	export LANGUAGE=en_AU:en_GB:en
-	export WINEPREFIX=/media/Data/NierAutomata/wine
-	cd /media/Data/NierAutomata
-	exec wine /media/Data/NierAutomata/NieRAutomata.exe
+	export WINEPREFIX=${HOME}/Games/NierAutomata/wine
+	cd ${HOME}/Games/NierAutomata
+	exec wine ${HOME}/Games/NierAutomata/NieRAutomata.exe
 elif [ $1 = "soaser" ]; then
 	export LANG=en_AU.UTF-8
 	export LANGUAGE=en_AU:en_GB:en
-	export WINEPREFIX=/media/Data2/Games/SoaSE
-	cd /media/Data2/Games/SoaSE
-	exec wine '/media/Data2/Games/SoaSE/Sins of a Solar Empire Rebellion.exe'
+	export WINEPREFIX=${HOME}/Games/SoaSE
+	cd ${HOME}/Games/SoaSE
+	exec wine "${HOME}/Games/SoaSE/Sins of a Solar Empire Rebellion.exe"
 fi
