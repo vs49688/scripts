90919f89daeeabdd90837afd42945279f8bc75de
Zane van Iperen <zane@zanevaniperen.com>
2020-08-28 16:41:57 +1000

    fuckcue.sh: fixes

--- fuckcue.sh
+++ fuckcue.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -e
+#set -x
+
+##
+# For reference:
+# IFS=$'\n'; for i in $(find . -name '*.cue'); do fuckcue.sh "$i" "${i::-4}.flac" "$(dirname "$i")/split"; done
+##
 
 if [ $# -ne 3 ]; then
 	echo "Usage: $0 <cuefile> <flacfile> <outdir>"
@@ -18,4 +26,4 @@ fi
 mkdir -p "$3"
 
 cuebreakpoints "$1" | shnsplit -O always -o flac -d "$3" "$2"
-cuetag "$1" $3/split-track*
+cuetag "$1" "$3"/split-track*
