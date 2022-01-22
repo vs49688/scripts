f3537af68ff94103ee2c066b86230f000b9bb048
Zane van Iperen <zane@zanevaniperen.com>
2022-01-22 19:31:06 +1000

    stuntxtract.py: add

--- README.md
+++ README.md
@@ -31,6 +31,7 @@ Various assorted scripts, published so I don't lose them. Use them as you will.
 | `mcmirror.py`               | Old-style Minecraft mirroring script. Ancient code, don't use. |
 | `pdfreduce.sh`              | Reduce the DPI of a PDF file. |
 | `cdtoc.py`                  | ~~Convert a CTDB TOCID to a MusicBrainz one.~~ Doesn't work. |
+| `stuntxtract.py`            | Extract RFH/RFD files from LEGO Stunt Rally. |
 
 ## License
 
---
+++ stuntxtract.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import struct
+import collections
+import os.path
+import subprocess
+import zlib
+
+ARCHIVES = [
+    'ART0001',
+    'CITY0001',
+    'DESERT0001',
+    'ICE0001',
+    'JUNGLE0001',
+    'SPEECH0001',
+    'TEXT0001',
+]
+
+SRIndexEntry = collections.namedtuple('SRIndexEntry', [
+    'unk1',
+    'unk2',
+    'unk3',
+    'size',
+    'offset',
+    'path'
+])
+
+index = {}
+
+for a in ARCHIVES:
+    with open(f'{a}.RFH', 'rb') as f:
+        data = f.read()
+
+    entries = []
+    while len(data) > 0:
+
+        fields = data[:20]
+        data = data[20:]
+
+        idx = data.index(b'\0')
+
+        unk1, unk2, unk3, size, offset = struct.unpack('<IIIII', fields)
+
+        name = data[:idx].decode('utf-8')
+        data = data[idx + 1:]
+
+        entries.append(SRIndexEntry(
+            unk1=unk1,
+            unk2=unk2,
+            unk3=unk3,
+            size=size,
+            offset=offset,
+            path=name
+        ))
+
+    index[a] = entries
+
+os.makedirs('ext', exist_ok=True)
+
+for a, entries in index.items():
+    with open(f'{a}.RFD', 'rb') as f:
+        data = f.read()
+
+    for entry in entries:
+        print(entry)
+        path = entry.path.replace('\\', '/')
+        dirname = os.path.dirname(path)
+
+        outpath = os.path.join('ext', path)
+        os.makedirs(os.path.join('ext', dirname), exist_ok=True)
+
+        filedata = data[entry.offset + 4:entry.offset + entry.size]
+        try:
+            decdata = zlib.decompress(filedata)
+        except zlib.error as e:
+            msg = e.args[0]
+
+            if msg.startswith('Error -3'):
+                pass
+            elif msg.startswith('Error -5'):
+                pass
+            else:
+                raise e
+
+            decdata = filedata
+
+        with open(outpath, 'wb') as f:
+            f.write(decdata)
